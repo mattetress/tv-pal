eaf96d938e19eb3f1ee09614bd475ee57748c22e
mattetress <mattetress@me.com>
2019-05-06 21:10:19 -0500

    generate favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_012222) do
+ActiveRecord::Schema.define(version: 2019_05_07_014627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tv_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "auth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
