86d367cd215ed3b0b969117b15ea47687cd670c9
mattetress <mattetress@me.com>
2019-05-08 14:16:56 -0500

    add Login Container

---
+++ client/src/containers/LoginContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginContainer extends Component {
+  render() {
+    return (
+      <div>
+        Login Container
+      </div>
+    )
+  }
+}
+
+export default LoginContainer;
