8561a91bce33fc2427293f322c9493044c54bd20
mattetress <mattetress@me.com>
2019-05-08 16:53:13 -0500

    add route for login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Landing from './components/Landing';
 import { connect } from 'react-redux';
-import RegistrationContainer from './containers/RegistrationContainer'
-import { Container } from 'react-bootstrap/Container'
+import RegistrationContainer from './containers/RegistrationContainer';
+import LoginContainer from './containers/LoginContainer';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <>
+        <React.Fragment>
           <Route exact path="/" component={Landing} />
           <Route exact path="/signup" component={RegistrationContainer} />
-        </>
+          <Route exact path="/login" component={LoginContainer} />
+        </React.Fragment>
       </Router>
     )
   }
