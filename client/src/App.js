34a04b120b1b045527a53c5056bb9612c9b50bb4
mattetress <mattetress@me.com>
2019-05-08 14:13:47 -0500

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import { connect } from 'react-redux';
+import RegistrationContainer from './containers/RegistrationContainer'
+import { Container } from 'react-bootstrap/Container'
 
-function App() {
-  return (
-    
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/signup" component={RegistrationContainer} />
+        </>
+      </Router>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(App);
