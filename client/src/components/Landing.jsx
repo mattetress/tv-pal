4c4fd4e7be902166b4b868258f04a8b900d59b35
mattetress <mattetress@me.com>
2019-05-09 01:13:12 -0500

    styling

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom'
+import "../Landing.css";
 
 export default function Landing() {
   return (
-    <Container>
-      <h1>TV Pal</h1>
-      <p>Please <Link to="/login">Log in</Link> or <Link to="/signup">Sign up</Link></p>
-    </Container>
+    <div className="bg">
+      <Container>
+        <div className="logo">
+          <h1>TV Pal</h1>
+          <p>Please <Link to="/login">Log in</Link> or <Link to="/signup">Sign up</Link></p>
+        </div>
+        <footer>Photo by Pablo García Saldaña on Unsplash</footer>
+      </Container>
+    </div>
   )
 }
 
