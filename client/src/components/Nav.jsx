d5642408f1e1b324d9c2ac28d1d1228fbf990aee
mattetress <mattetress@me.com>
2019-05-09 01:12:39 -0500

    add nav links

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import Row from 'react-bootstrap/Row'
+
+const Nav = () => {
+  return (
+    <Row className="menu bg-light">
+      <div className="col-12 mt-5">
+        <NavLink exact to="/shows/favorites" className="btn align-middle menu-link" activeClassName="btn-dark">My Shows</NavLink>
+      </div>
+      <div className="col-12">
+        <NavLink exact to="/shows/popular" className="btn menu-link" activeClassName="btn-dark">Popular</NavLink>
+      </div>
+      <div className="col-12">
+        <NavLink exact to="/shows/genres" className="btn menu-link" activeClassName="btn-dark">Genres</NavLink>
+      </div>
+    </Row>
+  )
+}
+
+export default Nav;
--- client/src/containers/Dashboard.jsx
+++ client/src/containers/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Nav />
       </div>
     )
   }
