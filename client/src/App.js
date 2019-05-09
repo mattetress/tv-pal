7388cd3cc78404699bfa4e96bb66dace12f02bca
mattetress <mattetress@me.com>
2019-05-09 05:57:57 -0500

    fix issue where wrong show page gets displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import { connect } from 'react-redux';
 import RegistrationContainer from './containers/RegistrationContainer';
 import LoginContainer from './containers/LoginContainer';
 import Dashboard from './containers/Dashboard'
 import PopularContainer from './containers/PopularContainer';
+import ShowContainer from './containers/ShowContainer';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <React.Fragment>
+        <Switch>
           <Route exact path="/" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <Landing />} />
           <Route exact path="/signup" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <RegistrationContainer />} />
           <Route exact path="/login" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <LoginContainer />} />
-          <Route path="/shows" render={() => (this.props.auth) ? <Dashboard /> : <Redirect to="/" />} />
-          <Route exact path="/shows/popular" render={() => <PopularContainer />} />
-        </React.Fragment>
+          <Route exact path="/shows/favorites" />
+          <Route exact path="/shows/popular" component={PopularContainer} />
+          <Route exact path="/shows/:id" component={ShowContainer} />
+        </Switch>
       </Router>
     )
   }
