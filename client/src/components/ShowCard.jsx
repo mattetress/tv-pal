df4f5acb441d1e0ffab71320b6a6ed8ec1f1e5a3
mattetress <mattetress@me.com>
2019-05-09 18:08:32 -0500

    remove unneeded div

--- client/src/components/ShowCard.jsx
+++ client/src/components/ShowCard.jsx
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 const ShowCard = props => {
   return (
 
-    <div className="col-3 p-2 card" >
-      <Link to={`/shows/${props.show.id}`} >
-        <div className="">
-          <img src={`https://image.tmdb.org/t/p/w200${props.show.poster_path}`} />
-          <p>{props.show.name}</p>
-        </div>
+    <div className={`${props.classes} p-2`} >
+      <Link key={props.show.id} to={`/shows/${props.show.id}`} >
+        <img src={`https://image.tmdb.org/t/p/w200${props.show.poster_path}`} />
+        <p>{props.show.name}</p>
       </Link>
     </div>
 
