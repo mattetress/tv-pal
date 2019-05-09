7bc70da222a4a760251cce62a10c7a5dad3847eb
mattetress <mattetress@me.com>
2019-05-08 22:14:28 -0500

    add path for shows

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import Landing from './components/Landing';
 import { connect } from 'react-redux';
 import RegistrationContainer from './containers/RegistrationContainer';
 import LoginContainer from './containers/LoginContainer';
+import Dashboard from './containers/Dashboard'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
-          <Route exact path="/" render={() => (this.props.auth) ? <Redirect to="/dashboard" /> : <Landing />} />
-          <Route exact path="/signup" render={() => (this.props.auth) ? <Redirect to="/dashboard" /> : <RegistrationContainer />} />
-          <Route exact path="/login" render={() => (this.props.auth) ? <Redirect to="/dashboard" /> : <LoginContainer />} />
+          <Route exact path="/" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <Landing />} />
+          <Route exact path="/signup" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <RegistrationContainer />} />
+          <Route exact path="/login" render={() => (this.props.auth) ? <Redirect to="/shows/favorites" /> : <LoginContainer />} />
+          <Route path="/shows" render={() => (this.props.auth) ? <Dashboard /> : <Redirect to="/" />} />
         </React.Fragment>
       </Router>
     )
