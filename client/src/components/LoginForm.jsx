a1a6d7f595e1ec9a8550b5fad6b18f144d4c0235
mattetress <mattetress@me.com>
2019-05-10 21:18:43 -0500

    add animation

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form';
+import { Spring } from 'react-spring/renderprops';
 
 class LoginForm extends Component {
   constructor() {
@@ -18,22 +19,39 @@ class LoginForm extends Component {
   }
 
   render() {
+
     return (
-      <div>
-        <h3>Log In</h3>
-        <Form onSubmit={event => this.props.handleSubmit(event, this.state)} >
-          <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control name="email" type="email" onChange={this.handleChange} placeholder="Enter email" value={this.state.email} required />
-          </Form.Group>
 
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" required />
-          </Form.Group>
+      <div className="bg">
+        <Spring
+          from={{
+            opacity: 0,
+            marginTop: -500
+          }}
+          to={{
+            opacity: 1,
+            marginTop: 150
+          }} >
+          {props => {
+            return (
+              <div style={props} className="form">
+                <h3>Log In</h3>
+                <Form onSubmit={event => this.props.handleSubmit(event, this.state)} >
+                  <Form.Group>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control name="email" type="email" onChange={this.handleChange} placeholder="Enter email" value={this.state.email} required />
+                  </Form.Group>
 
-          <Form.Control type="submit" className="btn btn-primary" value="Log In" />
-        </Form>
+                  <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" required />
+                  </Form.Group>
+
+                  <Form.Control type="submit" className="btn btn-primary" value="Log In" />
+                </Form>
+              </div>)
+          }}
+        </Spring>
       </div>
     )
 
--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form'
+import { Spring } from 'react-spring/renderprops'
 
 
 export default class RegistrationForm extends Component {
@@ -21,28 +22,42 @@ export default class RegistrationForm extends Component {
 
   render() {
     return (
-      <>
-        <h1>Sign Up</h1>
+      <div className="bg">
+        <Spring
+          from={{
+            opacity: 0,
+            marginTop: -500
+          }}
+          to={{
+            opacity: 1,
+            marginTop: 150
+          }} >
+          {props => (
+            <div style={props} className="form">
+              <h1>Sign Up</h1>
 
-        <Form onSubmit={event => this.props.handleSubmit(event, this.state)} >
-          <Form.Group>
-            <Form.Label>Email address</Form.Label>
-            <Form.Control required name="email" type="email" onChange={this.handleChange} placeholder="Enter email" value={this.state.email} />
-          </Form.Group>
+              <Form onSubmit={event => this.props.handleSubmit(event, this.state)} >
+                <Form.Group>
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control required name="email" type="email" onChange={this.handleChange} placeholder="Enter email" value={this.state.email} />
+                </Form.Group>
 
-          <Form.Group>
-            <Form.Label>Password</Form.Label>
-            <Form.Control required type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" />
-          </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control required type="password" name="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" />
+                </Form.Group>
 
-          <Form.Group>
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control required type="password" name="password_confirmation" onChange={this.handleChange} value={this.state.password_confirmation} placeholder="Re-enter password" />
-          </Form.Group>
+                <Form.Group>
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control required type="password" name="password_confirmation" onChange={this.handleChange} value={this.state.password_confirmation} placeholder="Re-enter password" />
+                </Form.Group>
 
-          <Form.Control className="btn btn-primary" type="submit" />
-        </Form>
-      </>
+                <Form.Control className="btn btn-primary" type="submit" />
+              </Form>
+            </div>)
+          } 
+          </Spring>
+      </div>
     )
   }
 }
