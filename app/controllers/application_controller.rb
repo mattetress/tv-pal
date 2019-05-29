3e21d1451c1325fca0cc8b9de24b3f861dfe3e3c
mattetress <mattetress@me.com>
2019-05-29 17:55:40 -0500

    add fallback route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  scope :api do 
+  scope :api do
     resources :users
-    
+
     post '/login' => 'sessions#create'
     delete '/logout' => 'sessions#destroy'
     get '/favorites' => 'users#favorites'
     post '/favorite/:id' => 'users#toggle_favorite'
+  end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
 end
